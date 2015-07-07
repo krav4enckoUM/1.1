23b61812b1238407415be1a766d8a9f4a0e8476f
krav4enckoUM <iurakrav4encko@yandex.ru>
2015-07-07 22:25:23 +0500

    код

---
+++ ConsoleApplication1задача 1/ConsoleApplication1задача 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System;
+
+namespace ConsoleApplication45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.Write("Объем: ");
+         double V = Convert.ToDouble(Console.ReadLine());
+         double r = Math.Pow(0.75*V/Math.PI, 0.333);
+         double S = 3*V/Math.Pow(0.75*V/Math.PI, 0.333);
+         Console.Write("Радиус = " + r + "\n" + "Площадь = " + S);
+         Console.ReadKey();
+        }
+    }
+}
